2db81a6ed4f9f5d402485a885d18d4da4e796a86
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-07-27 13:04:06 +0200

    feat(sample): sample flow first steps

--- sample/src/main/java/com/github/paylike/sample/sample.kt
+++ sample/src/main/java/com/github/paylike/sample/sample.kt
@@ -1,5 +1,17 @@
 package com.github.paylike.sample
 
+import com.github.paylike.kotlin_engine.engine.PaylikeEngine
+import com.github.paylike.kotlin_engine.service.ApiMode
+import kotlinx.coroutines.runBlocking
+
 fun main() {
     println("hello paylike engine")
+    val engine = PaylikeEngine("kotlinTestId01", ApiMode.TEST)
+    runBlocking {
+        engine.tokenize("4100000000000000", "111", 12, 2022)
+        println(engine.repository.cardRepository)
+    }
+
+
+
 }
