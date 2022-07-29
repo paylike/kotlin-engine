0a92e360b91b7bc0b185d27f86108cf615afb2b1
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-07-29 12:07:50 +0200

    feat(tdsWebView): groundwork for tds webview composable

---
+++ kotlin_engine/src/main/java/com/github/paylike/kotlin_engine/view/TdsWebvViewComposable.kt
@@ -0,0 +1,8 @@
+package com.github.paylike.kotlin_engine.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TdsWebView(htmlBody: String) {
+    // TODO webview build and give it htmlBody
+}
