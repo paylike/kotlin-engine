7923326de497b13319a122f268c800040137896b
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-09-12 16:08:00 +0200

    feat(readme): added readme content and a slight fix on docs

--- kotlin_engine/src/main/java/com/github/paylike/kotlin_engine/viewmodel/EngineState.kt
+++ kotlin_engine/src/main/java/com/github/paylike/kotlin_engine/viewmodel/EngineState.kt
@@ -1,12 +1,13 @@
 package com.github.paylike.kotlin_engine.viewmodel
 
 /**
- * [WAITING_FOR_INPUT]
- * - initial state [WEBVIEW_CHALLENGE_STARTED]
- * - the first html response has arrived [WEBVIEW_CHALLENGE_USER_INPUT_REQUIRED]
- * - the html form is rendered [SUCCESS]
- * - successful TDS flow [ERROR]
- * - at any point of the payment flow there are error branches to run into.
+ * The engine is one of the following states all the time:
+ * - initial state: [WAITING_FOR_INPUT]
+ * - the first html response has arrived: [WEBVIEW_CHALLENGE_STARTED]
+ * - the html form is rendered: [WEBVIEW_CHALLENGE_USER_INPUT_REQUIRED]
+ * - successful TDS flow: [SUCCESS]
+ * - in any case of error: [ERROR]
+ * - At any point of the payment flow there are error branches to run into.
  */
 enum class EngineState {
     WAITING_FOR_INPUT,
